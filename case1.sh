03bff1f20d47f91700a98d90730e5e5682d372d9
shashank <shashank.w1998@gmail.com>
2021-02-16 14:17:30 +0530

    case3 code added

--- case1.sh
+++ case1.sh
@@ -13,3 +13,21 @@ fi
 wage_per_hr=20
 daily_wage=$(($wage_per_hr*8))
 echo "daily wage:" $daily_wage
+
+part_time=1
+full_time=2
+absent=0
+part_time_hr=4
+wage_per_hr=20
+full_time_hr=8
+randomNumber=$(($RANDOM%3))
+if [ $randomNumber -eq $part_time ]
+then
+	echo "part time wage:" $(($part_time_hr*$wage_per_hr))
+elif [ $randomNumber -eq $full_time ]
+then
+	echo "full time wage:" $(($full_time_hr*$wage_per_hr))
+else
+	echo "worker absent"
+fi
+
