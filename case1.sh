0043550bc255334dc3e689fd3eeb4c582e76c87a
shashank <shashank.w1998@gmail.com>
2021-02-16 14:30:51 +0530

    case456 code added

--- case1.sh
+++ case1.sh
@@ -31,3 +31,17 @@ else
 	echo "worker absent"
 fi
 
+daily_wage=160
+wage_per_hr=20
+echo "1:Wage per month(by day) 2:wage per month(by hr and day)"
+read -p "enter choice:" choice
+case $choice in
+   "1")
+      monthly_wage=$((20*$daily_wage))
+      echo "monthly  wage(by days):" $monthly_wage
+      ;;
+   "2")
+      monthly_wage=$((100*$wage_per_hr))
+      echo "monthly wage(by hours):" $monthly_wage
+      ;;
+esac
